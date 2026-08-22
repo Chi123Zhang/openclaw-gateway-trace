f9443c62d3496e743653bf518903585724b59678
Chi Zhang <cz2925@columbia.edu>
2026-08-23 05:06:13 +0800

    Add trace case index

---
+++ data/cases/index.js
@@ -0,0 +1,8 @@
+window.GATEWAY_CASE_INDEX = [
+  {
+    "id": "cake",
+    "title": "How to make a cake?",
+    "file": "data/cases/cake.js",
+    "description": "Current OpenClaw Gateway trace used to build the dashboard."
+  }
+];
